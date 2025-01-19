20ecc43eb509a44e91e82607f756eee48b8446c5
poojapatil <156627026+patil-poooja@users.noreply.github.com>
2025-01-19 14:01:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let res = null;
+	for (let char of str) {
+    if (
+        str.indexOf(char) ===        str.lastIndexOf(char)
+    ) {
+        res = char;
+        break;
+    }
+}
+//console.lo("Input: " + str);
+return res;
 }
 const input = prompt("Enter a string");
+
 alert(firstNonRepeatedChar(input)); 
